f27345d987a0f6dc42e8886838608dfbbd29d149
stefaniesola <stefaniedeleu846@gmail.com>
2026-02-01 18:58:51 +0100

    Update homepage pillar image

--- src/data/content.ts
+++ src/data/content.ts
@@ -589,7 +589,7 @@ export const homeCopy = {
     {
       title: "Ervaringsgericht leren in kleine groepen",
       description: "Binnen de veilige omgeving van een kleine groep ga je tijdens de reis actief aan de slag met de opgebouwde kennis. Je toetst de theorie aan de praktijk en leert technieken die je via herhaling helpen om echte verandering in gang te zetten. Je gaat terug naar huis met een stevig pakket handvatten waar je op kunt teruggrijpen voor houvast en continuïteit.",
-      image: "/src/assets/images/2631991747.jpg",
+      image: "/src/assets/images/Verblijf in Hoge Venen.avif",
     },
     {
       title: "Beweging met beide voeten in de natuur",
